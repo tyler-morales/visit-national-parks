0a9e1f466737c49aa3d308c6e748314f4a5c6395
tyler-morales <tyler.morales.dev@gmail.com>
2021-12-07 14:33:48 -0600

    📦  Setup Tailwind CSS

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,33 @@
 export default function Home() {
-  return <div>hello world</div>
+  return (
+    <div>
+      <h1 class="bg-yellow-400 mt-[50px] text-3xl">Hello world</h1>
+      <p class="font-thin">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At dolorem
+        ipsam corporis earum repudiandae ipsa ut quasi perferendis
+        exercitationem laborum.
+      </p>
+      <p class="font-light">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At dolorem
+        ipsam corporis earum repudiandae ipsa ut quasi perferendis
+        exercitationem laborum.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At dolorem
+        ipsam corporis earum repudiandae ipsa ut quasi perferendis
+        exercitationem laborum.
+      </p>
+
+      <p class="font-bold">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At dolorem
+        ipsam corporis earum repudiandae ipsa ut quasi perferendis
+        exercitationem laborum.
+      </p>
+      <p class="font-black">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At dolorem
+        ipsam corporis earum repudiandae ipsa ut quasi perferendis
+        exercitationem laborum.
+      </p>
+    </div>
+  )
 }

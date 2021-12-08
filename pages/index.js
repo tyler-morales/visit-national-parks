0e19fcf17ea3a26fcba6e64e2ad8b1d80a9c417e
tyler-morales <tyler.morales.dev@gmail.com>
2021-12-07 19:00:17 -0600

    ✏️  Add typefaces

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 class="bg-yellow-400 mt-[50px] text-3xl">Hello world</h1>
-      <p class="font-thin">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At dolorem
-        ipsam corporis earum repudiandae ipsa ut quasi perferendis
-        exercitationem laborum.
-      </p>
-      <p class="font-light">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At dolorem
-        ipsam corporis earum repudiandae ipsa ut quasi perferendis
-        exercitationem laborum.
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At dolorem
-        ipsam corporis earum repudiandae ipsa ut quasi perferendis
-        exercitationem laborum.
-      </p>
-
-      <p class="font-bold">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At dolorem
-        ipsam corporis earum repudiandae ipsa ut quasi perferendis
-        exercitationem laborum.
-      </p>
-      <p class="font-black">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At dolorem
-        ipsam corporis earum repudiandae ipsa ut quasi perferendis
-        exercitationem laborum.
-      </p>
+      <h1 className="text-5xl font-bold">Visit National Parks!</h1>
     </div>
   )
 }

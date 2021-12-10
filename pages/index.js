ae9cd1d05f6084e4996b2f48846212b9ebf3c47e
tyler-morales <tyler.morales.dev@gmail.com>
2021-12-09 20:10:07 -0600

    📦  Add Cypress Run a demo test to navigate to the about page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import Link from 'next/link'
+
 import {Pill} from '../components/Pill/Pill'
 
 export default function Home() {
   return (
     <div>
+      <nav>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </nav>
       <h1 className="text-5xl font-bold">Visit National Parks!</h1>
       <Pill label="Welcome" emoji="😊" />
     </div>

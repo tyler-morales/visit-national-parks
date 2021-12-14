3718e12b337900589cd9f0268cae08b6acd4469e
tyler-morales <tyler.morales.dev@gmail.com>
2021-12-14 11:31:30 -0600

    📦  Initalize Amplify again

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,15 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "visitnationalparksb8e6205d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ configureAmplify.js
@@ -0,0 +1,3 @@
+import Amplify from '@aws-amplify/core'
+import config from './aws-exports'
+Amplify.configure(config)

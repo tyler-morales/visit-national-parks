7cc6aba74558827982d9941a8bad24add1a6185a
tyler-morales <tyler.morales.dev@gmail.com>
2021-12-26 13:49:25 -0600

    🔥 Take out SWR data Will populate in new branch

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import {Nav} from '../components/Nav/Nav'
 import SearchBar from '../components/SearchBar/SearchBar'
 
 export default function Home() {
-  // console.log(parks)
   return (
     <div>
       <Nav />
@@ -12,21 +11,3 @@ export default function Home() {
     </div>
   )
 }
-
-// export async function getStaticProps() {
-//   // console.log(process.env.API_KEY)
-//   const res = await fetch(
-//     `https://developer.nps.gov/api/v1/parks?parkCode=yell&api_key=${process.env.API_KEY}`
-//   )
-//   const data = await res.json()
-
-//   if (!data) {
-//     return {
-//       notFound: true,
-//     }
-//   }
-
-//   return {
-//     props: {parks: data}, // will be passed to the page component as props
-//   }
-// }

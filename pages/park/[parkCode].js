19df6537e86d8d5752419d5017ff671d15aa8730
tyler-morales <tyler.morales.dev@gmail.com>
2021-12-27 15:48:59 -0600

    🐛  Fix figcaption trailing on next column

--- pages/park/[parkCode].js
+++ pages/park/[parkCode].js
@@ -35,10 +35,12 @@ export default function Park({park}) {
       <h3 className="mt-24 text-3xl font-bold text-green-800 mb-7">
         More Images
       </h3>
-      <div className="gap-10 columns-3">
+      <div className="gap-10 columns-1 sm:columns-2 md:columns-3">
         {images.map((img, index) => {
           return (
-            <figure key={index} className="mb-10 text-center">
+            <figure
+              key={index}
+              className="mb-10 text-center break-inside-avoid-column">
               <img className="rounded-xl" src={img.url} alt={img.altText} />
               <figcaption className="mt-4 text-sm italic text-gray-600 ">
                 {img.altText}

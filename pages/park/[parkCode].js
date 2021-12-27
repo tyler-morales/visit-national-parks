9887a475ebdca1ea4507ebc2c02ba89ef3b6997a
tyler-morales <tyler.morales.dev@gmail.com>
2021-12-27 15:55:17 -0600

    ♿️  Add semantic markup

--- pages/park/[parkCode].js
+++ pages/park/[parkCode].js
@@ -5,7 +5,7 @@ export default function Park({park}) {
   const {url, altText, caption, credit} = images[0]
 
   return (
-    <div className="max-w-[1080px] m-auto">
+    <main className="max-w-[1080px] m-auto">
       <span className="block mb-2 text-center">{designation}</span>
       <h1 className="mb-5 font-bold text-center text-green-800 text-7xl">
         {name}
@@ -32,10 +32,11 @@ export default function Park({park}) {
       <h3 className="mb-3 text-3xl font-bold text-green-800">Overview</h3>
       <p className="mt-4">{description}</p>
 
+      {/* Images */}
       <h3 className="mt-24 text-3xl font-bold text-green-800 mb-7">
         More Images
       </h3>
-      <div className="gap-10 columns-1 sm:columns-2 md:columns-3">
+      <section className="gap-10 columns-1 sm:columns-2 md:columns-3">
         {images.map((img, index) => {
           return (
             <figure
@@ -48,8 +49,8 @@ export default function Park({park}) {
             </figure>
           )
         })}
-      </div>
-    </div>
+      </section>
+    </main>
   )
 }
 
@@ -85,7 +86,6 @@ export async function getStaticProps(context) {
   const URL = 'https://developer.nps.gov/api/v1/'
 
   const {params} = context
-  // console.log(params)
 
   const res = await fetch(
     `${URL}parks?parkCode=${params.parkCode}&limit=465&api_key=${process.env.API_KEY}`,

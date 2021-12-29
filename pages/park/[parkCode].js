2487286be7794dfff85acec1477defcc3f5125de
tyler-morales <tyler.morales.dev@gmail.com>
2021-12-29 01:39:05 -0600

    🚧  Design Bookmark button

--- pages/park/[parkCode].js
+++ pages/park/[parkCode].js
@@ -1,11 +1,34 @@
+import {useState, useEffect} from 'react'
 import {Parallax} from 'react-parallax'
+import {AiFillCaretDown} from 'react-icons/ai'
+
 import Layout from '../../components/Layout'
 import {useRouter} from 'next/router'
+import {MdBookmark, MdBookmarkBorder} from 'react-icons/md'
+import {BsCheckLg} from 'react-icons/bs'
+
+const collectionValues = {
+  visited: 'VISITED',
+  bookmark: 'BOOKMARK',
+}
 
 export default function Park({park}) {
   const router = useRouter()
   const {name, description, designation, images} = park?.data[0]
   const {url, altText, caption, credit} = images[0]
+  const [selectedCollection, setCollection] = useState(null)
+  const [toggleDropdown, setToggleDropdown] = useState(false)
+
+  const openDropdown = () => {
+    if (toggleDropdown) {
+      setToggleDropdown(false)
+    } else {
+      setToggleDropdown(true)
+    }
+  }
+
+  // Close dropdown when user interacts with it
+  useEffect(() => setToggleDropdown(false), [selectedCollection])
 
   return (
     <Layout>
@@ -14,6 +37,8 @@ export default function Park({park}) {
         className="text-sm text-gray-600-700">
         ↞Back to Results
       </button>
+
+      {/* Title */}
       <span className="block mb-2 text-center">{designation}</span>
       <h1 className="mb-5 font-bold text-center text-green-800 text-7xl">
         {name}
@@ -35,6 +60,85 @@ export default function Park({park}) {
       </figure>
 
       <hr className="my-12 border-gray-400" />
+
+      {/* Collection */}
+      {/* focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none */}
+      <div className="relative my-8 w-max">
+        {/* Button */}
+        <div className="flex">
+          {selectedCollection == null && (
+            <button
+              onClick={() => setCollection('BOOKMARK')}
+              className="relative flex items-center gap-3 text-white bg-blue-600 rounded-l-md hover:bg-blue-700">
+              <span className="flex items-center gap-3 px-4">
+                <MdBookmarkBorder />
+                <span>Want to Visit</span>
+              </span>
+            </button>
+          )}
+
+          {selectedCollection == 'VISITED' && (
+            <button
+              onClick={() => setCollection(null)}
+              className="flex items-center gap-3 px-4 py-2 pr-6 text-white bg-green-600 rounded-md hover:bg-green-700">
+              <BsCheckLg />
+              <span>Visited</span>
+            </button>
+          )}
+
+          {selectedCollection == 'BOOKMARK' && (
+            <button
+              onClick={() => setCollection(null)}
+              className="relative flex items-center gap-3 text-white bg-blue-600 rounded-l-md hover:bg-blue-700">
+              <span className="flex items-center gap-3 px-4">
+                <MdBookmark />
+                <span>Bookmarked</span>
+              </span>
+            </button>
+          )}
+
+          {/* Arrow */}
+          {selectedCollection != 'VISITED' && (
+            <button
+              onClick={openDropdown}
+              className={`px-4 py-3 pl-4 text-white rounded-r-md  ${
+                selectedCollection == 'VISITED'
+                  ? 'bg-green-600 hover:bg-green-700'
+                  : 'bg-blue-600 hover:bg-blue-700'
+              }`}>
+              <AiFillCaretDown />
+            </button>
+          )}
+        </div>
+
+        {/* Dropdown */}
+        {selectedCollection == null && (
+          <button
+            onClick={() => setCollection('VISITED')}
+            className={`absolute items-center w-full gap-3 mt-2 text-black bg-blue-300 rounded-md hover:bg-blue-400`}>
+            <span
+              className={`flex items-center gap-3 px-4 ${
+                toggleDropdown ? 'flex' : 'hidden'
+              }`}>
+              <BsCheckLg />
+              <span className="py-2">Visited</span>
+            </span>
+          </button>
+        )}
+        {selectedCollection == 'BOOKMARK' && (
+          <button
+            onClick={() => setCollection('VISITED')}
+            className={`absolute items-center w-full gap-3 mt-2 text-black bg-blue-300 rounded-md hover:bg-blue-400`}>
+            <span
+              className={`flex items-center gap-3 px-4 ${
+                toggleDropdown ? 'flex' : 'hidden'
+              }`}>
+              <BsCheckLg />
+              <span className="py-2">Visited</span>
+            </span>
+          </button>
+        )}
+      </div>
 
       {/* Description */}
       <h3 className="mb-3 text-3xl font-bold text-green-800">Overview</h3>

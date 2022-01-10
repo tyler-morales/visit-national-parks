582a82cb123075a585e33a8a69733b6d095e84b3
tyler-morales <tyler.morales.dev@gmail.com>
2022-01-09 18:46:04 -0600

    💄 Style link tags to individual parks

--- pages/profile.js
+++ pages/profile.js
@@ -5,6 +5,8 @@ import Avatar from 'boring-avatars'
 import {RiDeleteBinLine} from 'react-icons/ri'
 import {RiEdit2Line} from 'react-icons/ri'
 
+import Link from 'next/link'
+
 import {listSites} from '../src/graphql/queries'
 
 function Profile({username, email, name, bio, visitedSites, bookmarkedSites}) {
@@ -30,16 +32,26 @@ function Profile({username, email, name, bio, visitedSites, bookmarkedSites}) {
     return (
       <tr className="w-full">
         <td data-th="Image" className="text-base font-thin text-left">
-          <img
-            src={site.img}
-            alt={site.fullName}
-            className="rounded-lg w-[150px] h-auto"
-          />
+          <Link href={`/park/${site.code}`}>
+            <a>
+              <img
+                src={site.img}
+                alt={site.fullName}
+                className="rounded-lg w-[150px] h-auto"
+              />
+            </a>
+          </Link>
         </td>
         <td
           data-th="Name"
           className="max-w-[150px] text-base font-thin text-left">
-          <span className="text-lg font-bold text-green-800">{site.name}</span>
+          <Link href={`/park/${site.code}`}>
+            <a>
+              <span className="text-lg font-bold text-green-800 hover:underline hover:underline-offset-4 hover:decoration-wavy">
+                {site.name}
+              </span>
+            </a>
+          </Link>
         </td>
         <td data-th="Name" className="text-base font-thin text-left ">
           <span className="text-lg font-bold text-green-800">N/A</span>
@@ -54,7 +66,7 @@ function Profile({username, email, name, bio, visitedSites, bookmarkedSites}) {
           <span className="text-lg font-bold text-green-800">N/A</span>
         </td>
         <td data-th="Settings" className="text-base font-thin text-left">
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 text-gray-700">
             <button className="flex items-center w-full gap-2 items-between text-small">
               <RiEdit2Line size="1.25em" />
               <span>Edit</span>
@@ -197,7 +209,7 @@ function Profile({username, email, name, bio, visitedSites, bookmarkedSites}) {
             </span>
           </div>
         </section>
-        <section className="w-full col-span-3 border">
+        <section className="w-full col-span-3">
           <div className="flex gap-8">
             <button className="px-4 py-2 text-2xl font-bold bg-orange-200 rounded-lg">
               Visited

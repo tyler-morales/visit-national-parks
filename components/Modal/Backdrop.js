d8eacc72230ae23a5da3d5816aec8a817b38ffee
tyler-morales <tyler.morales.dev@gmail.com>
2022-01-12 11:29:53 -0600

    🐛  Fit backgrop overlay to conver entire background Fixes bug when user scrolls the backdrop would not fit

--- components/Modal/Backdrop.js
+++ components/Modal/Backdrop.js
@@ -1,7 +1,7 @@
 const Backdrop = ({children, onClick}) => {
   return (
     <div
-      className="absolute top-0 left-0 z-10 flex items-center justify-center w-full h-screen bg-[#000000e1]"
+      className="fixed top-0 left-0 z-10 flex items-center justify-center w-full h-full bg-[#000000e1]"
       onClick={onClick}>
       {children}
     </div>

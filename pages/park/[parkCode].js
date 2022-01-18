d0d3153eea27603dd9bf4257077fa4f0316564eb
tyler-morales <tyler.morales.dev@gmail.com>
2022-01-18 15:04:08 -0600

    🚧  Create bookmark button

--- pages/park/[parkCode].js
+++ pages/park/[parkCode].js
@@ -1,18 +1,21 @@
 import {useState, useEffect} from 'react'
 import {Parallax} from 'react-parallax'
-import {AiFillCaretDown} from 'react-icons/ai'
-import {v4 as uuidv4} from 'uuid'
 import Layout from '../../components/Layout'
 import {useRouter} from 'next/router'
+import {AiFillCaretDown} from 'react-icons/ai'
+import {v4 as uuidv4} from 'uuid'
+
+import checkUser from '../../hooks/checkUser'
+import {API, graphqlOperation} from 'aws-amplify'
+
 import {MdBookmark, MdBookmarkBorder} from 'react-icons/md'
 import {BsCheckLg} from 'react-icons/bs'
 
-import checkUser from '../../hooks/checkUser'
-
-// GraphQL
-import {API, Auth, withSSRContext} from 'aws-amplify'
+import useSWR from 'swr'
 import {createSite, updateSite} from '../../src/graphql/mutations'
-import {listSites} from '../../src/graphql/queries'
+import {listSites, getSite} from '../../src/graphql/queries'
+import {onCreateSite, onUpdateSite} from '../../src/graphql/subscriptions'
+// import CollectionButton from '../../components/CollectionButton/CollectionButton'
 
 export default function Park({
   name,
@@ -21,31 +24,97 @@ export default function Park({
   parkCode,
   designation,
   images,
-  allSites,
 }) {
-  const user = checkUser()
   const router = useRouter()
-
-  if (router.isFallback) {
-    return 'loading'
-  }
-
-  // Get updated data
-  const refreshData = () => {
-    router.replace({pathname: router.asPath}, undefined, {scroll: false})
-  }
-
-  // useEffect(() => {
-  //   refreshData()
-  // }, [])
+  const user = checkUser()
 
   const [toggleDropdown, setToggleDropdown] = useState(false)
-  const [selectedCollection, setCollection] = useState(null)
+  const [visited, setVisited] = useState(null)
+  const [bookmarked, setBookmarked] = useState(null)
 
-  const {url, altText, caption, credit} = images[0]
+  // console.log({bookmarked, visited})
 
-  // Filter parks that match parkpage to the logged-in user
-  let filteredSite = allSites?.owner == user?.username ? allSites : null
+  // Once the  user data loads, call this function to populate the Collection button with the correct label ('Want to visit' or 'visited')
+  const fetchUserSite = async (owner, code) => {
+    try {
+      const {data} = await API.graphql({
+        query: listSites,
+        variables: {
+          filter: {
+            code: {eq: code},
+            owner: {eq: owner},
+          },
+        },
+      })
+
+      const userData = data?.listSites?.items[0]
+
+      // Site is bookmarked, set UI to bookmarked
+      if (userData?.bookmarked) {
+        setBookmarked(true)
+      }
+
+      // Site is visited, set UI to visited
+      if (userData?.visited) {
+        setVisited(true)
+      }
+
+      // Create site object if no site exists
+      if (userData == null) {
+        setBookmarked(false)
+        setVisited(false)
+
+        const siteInfo = {
+          id: uuidv4(),
+          name: fullName,
+          code: parkCode,
+          img: url,
+          owner: user?.username,
+          bookmarked: false,
+          visited: false,
+        }
+
+        await API.graphql({
+          query: createSite,
+          variables: {input: siteInfo},
+          authMode: 'AMAZON_COGNITO_USER_POOLS',
+        })
+
+        setBookmarked(false)
+        setVisited(false)
+
+        console.log(`${name} added for the first time`)
+      }
+      return userData
+    } catch (err) {
+      console.log('Site not added by user', data, err)
+    }
+  }
+
+  // Only call the fetchUserSite method if `user` exists
+  const {data} = useSWR(user ? [user?.username, parkCode] : null, fetchUserSite)
+
+  useEffect(() => {
+    subscribe(parkCode, user?.username)
+  }, [user])
+
+  async function subscribe(code, owner) {
+    console.log(code, owner)
+    await API.graphql({query: onCreateSite}).subscribe({
+      next: (siteData) => {
+        console.log(
+          'Subscribed to data',
+          siteData?.value?.data?.onCreateSite?.id
+        )
+      },
+    })
+  }
+
+  // if (data) {
+  //   console.log(data?.id)
+  // } else {
+  //   console.log('waiting for data', error)
+  // }
 
   const openDropdown = () => {
     if (!user) alert('Please sign in or create an account')
@@ -58,43 +127,15 @@ export default function Park({
       }
     }
   }
-
   const toggleVisitedQuery = async () => {
-    console.log('clicked')
-
-    if (filteredSite?.visited) {
-      setCollection('visited')
-    } else if (filteredSite?.visited == false) {
-      setCollection('UNvisited')
-    } else if (filteredSite?.visited == null) setCollection(null)
-
     try {
-      const siteInfo = {
-        id: uuidv4(),
-        code: parkCode,
-        owner: user?.username,
-        name: fullName,
-        img: url,
-        bookmarked: false,
-        visited: true,
-      }
-      // A site does not exist, create a new entry
-      if (filteredSite == null) {
-        await API.graphql({
-          query: createSite,
-          variables: {input: siteInfo},
-          authMode: 'AMAZON_COGNITO_USER_POOLS',
-        })
-        refreshData()
-        console.log(`${name} added`)
-      }
       // A site exists, update it
-      if (filteredSite?.visited == false || filteredSite?.visited == null) {
+      if (!visited) {
         await API.graphql({
           query: updateSite,
           variables: {
             input: {
-              id: filteredSite.id,
+              id: data.id,
               visited: true,
               bookmarked: false,
               owner: user?.username,
@@ -102,8 +143,8 @@ export default function Park({
           },
           authMode: 'AMAZON_COGNITO_USER_POOLS',
         })
-        refreshData()
-        setCollection('VISITED')
+        setVisited(true)
+        setBookmarked(false)
         console.log(`${name} visited`)
       }
     } catch (err) {
@@ -111,97 +152,75 @@ export default function Park({
     }
   }
 
+  // When the user clicks on the button, update the state in the UI and database
   const handleDBQuery = async () => {
-    if (filteredSite?.bookmarked) {
-      setCollection('BOOKMARKED')
-    } else if (filteredSite?.bookmarked == false) {
-      setCollection('UNBOOKMARKED')
-    } else if (filteredSite?.bookmarked == null) setCollection(null)
-
     try {
-      user
-        ? setCollection('BOOKMARK')
-        : alert('Please sign in or create an account')
-
-      const siteInfo = {
-        id: uuidv4(),
-        name: fullName,
-        code: parkCode,
-        img: url,
-        bookmarked: true,
-        owner: user?.username,
+      // Make user log in if they are not already
+      if (user) {
+        setBookmarked(false)
+      } else {
+        alert('Please sign in or create an account')
       }
 
-      // A site does not exist, create a new entry
-      if (filteredSite == null) {
+      // BOOKMARK SITE
+      if (!bookmarked) {
         await API.graphql({
-          query: createSite,
-          variables: {input: siteInfo},
+          query: updateSite,
+          variables: {
+            input: {
+              id: data?.id,
+              bookmarked: true,
+              owner: user?.username,
+            },
+          },
           authMode: 'AMAZON_COGNITO_USER_POOLS',
         })
-        refreshData()
-        console.log(`${name} added`)
+        setVisited(false)
+        setBookmarked(true)
+        console.log(`${name} Bookmarked`)
       }
-      // A site exists, update it
-      else {
-        // BOOKMARK SITE
-        if (!filteredSite?.bookmarked && !filteredSite?.visited) {
-          await API.graphql({
-            query: updateSite,
-            variables: {
-              input: {
-                id: filteredSite?.id,
-                bookmarked: true,
-                owner: user?.username,
-              },
+      // REMOVE BOOKMARK SITE
+      if (bookmarked) {
+        await API.graphql({
+          query: updateSite,
+          variables: {
+            input: {
+              id: data?.id,
+              bookmarked: false,
             },
-            authMode: 'AMAZON_COGNITO_USER_POOLS',
-          })
-          refreshData()
-          setCollection('BOOKMARK')
-          console.log(`${name} Bookmarked`)
-        }
-        // REMOVE BOOKMARK SITE
-        if (filteredSite?.bookmarked) {
-          await API.graphql({
-            query: updateSite,
-            variables: {
-              input: {
-                id: filteredSite?.id,
-                bookmarked: false,
-              },
+          },
+          authMode: 'AMAZON_COGNITO_USER_POOLS',
+        })
+        setBookmarked(false)
+        console.log(`${name} Unbookmarked`)
+      }
+      // REMOVE VISIT
+      if (visited) {
+        console.log('unvisiting')
+        await API.graphql({
+          query: updateSite,
+          variables: {
+            input: {
+              id: data?.id,
+              visited: false,
+              bookmarked: false,
             },
-            authMode: 'AMAZON_COGNITO_USER_POOLS',
-          })
-          refreshData()
-          setCollection(null)
-          console.log(`${name} Unbookmarked`)
-        }
-        // REMOVE VISIT
-        if (filteredSite?.visited) {
-          console.log('unvisiting')
-          await API.graphql({
-            query: updateSite,
-            variables: {
-              input: {
-                id: filteredSite?.id,
-                visited: false,
-                bookmarked: false,
-              },
-            },
-            authMode: 'AMAZON_COGNITO_USER_POOLS',
-          })
-          refreshData()
-          setCollection(null)
-        }
+          },
+          authMode: 'AMAZON_COGNITO_USER_POOLS',
+        })
+        setVisited(false)
+        setBookmarked(false)
       }
     } catch (err) {
       console.error(err)
     }
   }
 
-  // Close dropdown when user interacts with it
-  useEffect(() => setToggleDropdown(false), [filteredSite])
+  if (router.isFallback) {
+    return 'loading...'
+  }
+
+  const {url, altText, caption, credit} = images[0]
 
   return (
     <Layout>
@@ -238,7 +257,7 @@ export default function Park({
       <div className="relative my-8 w-max">
         {/* Button */}
         <div className="flex">
-          {!filteredSite?.bookmarked && !filteredSite?.visited && (
+          {!bookmarked && !visited && (
             <button
               onClick={handleDBQuery}
               className="relative flex items-center gap-3 text-white bg-blue-600 rounded-l-md hover:bg-blue-700 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
@@ -249,7 +268,7 @@ export default function Park({
             </button>
           )}
 
-          {filteredSite?.bookmarked && filteredSite?.owner == user?.username && (
+          {bookmarked && (
             <button
               onClick={handleDBQuery}
               className="relative flex items-center gap-3 text-white bg-blue-600 rounded-l-md hover:bg-blue-700 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
@@ -260,7 +279,7 @@ export default function Park({
             </button>
           )}
 
-          {filteredSite?.visited && (
+          {visited && (
             <button
               onClick={handleDBQuery}
               className="flex items-center gap-3 px-4 py-2 pr-6 text-white bg-green-600 rounded-md hover:bg-green-700 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
@@ -270,11 +289,11 @@ export default function Park({
           )}
 
           {/* Arrow */}
-          {!filteredSite?.visited && (
+          {!visited && (
             <button
               onClick={openDropdown}
               className={`px-4 py-3 pl-4 text-white rounded-r-md focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none ${
-                selectedCollection == 'VISITED'
+                visited
                   ? 'bg-green-600 hover:bg-green-700'
                   : 'bg-blue-600 hover:bg-blue-700'
               }`}>
@@ -284,20 +303,20 @@ export default function Park({
         </div>
 
         {/* Dropdown */}
-        {(selectedCollection == null || selectedCollection == 'BOOKMARK') && (
-          <button
-            // onClick={() => setCollection('VISITED')}
-            onClick={toggleVisitedQuery}
-            className={`shadow-lg absolute items-center w-full gap-3 mt-2 text-black bg-blue-300 rounded-md hover:bg-blue-400 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none`}>
-            <span
-              className={`flex items-center gap-3 px-4 ${
-                toggleDropdown ? 'flex' : 'hidden'
-              }`}>
-              <BsCheckLg />
-              <span className="py-2">Visited</span>
-            </span>
-          </button>
-        )}
+        {bookmarked ||
+          (!bookmarked && !visited && (
+            <button
+              onClick={toggleVisitedQuery}
+              className={`shadow-lg absolute items-center w-full gap-3 mt-2 text-black bg-blue-300 rounded-md hover:bg-blue-400 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none`}>
+              <span
+                className={`flex items-center gap-3 px-4 ${
+                  toggleDropdown ? 'flex' : 'hidden'
+                }`}>
+                <BsCheckLg />
+                <span className="py-2">Visited</span>
+              </span>
+            </button>
+          ))}
       </div>
 
       {/* Description */}
@@ -358,16 +377,6 @@ export async function getStaticProps({params}) {
 
   const {name, description, parkCode, designation, images, fullName} = park
 
-  const {data} = await API.graphql({
-    query: listSites,
-    variables: {
-      id: params.parkCode,
-      filter: {name: {eq: params.parkCode}},
-    },
-  })
-
-  const allSites = data?.listSites?.items[0] || null
-
   return {
     props: {
       name,
@@ -376,8 +385,22 @@ export async function getStaticProps({params}) {
       parkCode,
       designation,
       images,
-      allSites,
     },
-    revalidate: 10,
+    revalidate: 1,
   }
+}
+
+const handleDBQuery = async () => {
+  await API.graphql({
+    query: updateSite,
+    variables: {
+      input: {
+        id: data?.id,
+        bookmarked: true,
+        owner: user?.username,
+      },
+    },
+    authMode: 'AMAZON_COGNITO_USER_POOLS',
+  })
+  console.log(`${name} Bookmarked`)
 }

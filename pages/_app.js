21f409d37acfa32f1a247f52b91e8eefd35e5a3a
tyler-morales <tyler.morales.dev@gmail.com>
2022-01-20 11:25:40 -0600

    🔍  Add head tag - Add a dynamic title to each park page - Add default meta tags for the entire site on _app.js - Add a meta description for park pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 import {Nav} from '../components/Nav/Nav'
+import Head from 'next/head'
 
 import Amplify from 'aws-amplify'
 import awsconfig from '../aws-exports'
@@ -10,6 +11,10 @@ Amplify.configure({...awsconfig, ssr: true})
 function MyApp({Component, pageProps}) {
   return (
     <>
+      <Head>
+        <title>Parkway | Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Nav />
       <Component {...pageProps} />
     </>

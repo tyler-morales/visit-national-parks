46b82953122c96509b4248671e080e7c243dd8a0
tyler-morales <tyler.morales.dev@gmail.com>
2022-02-11 14:44:13 -0600

    📱 Make nav responsive

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import {useState, useEffect, useLayoutEffect} from 'react'
+import {useState, useEffect} from 'react'
 import Link from 'next/link'
 import {Auth} from 'aws-amplify'
 import {Cross as Hamburger} from 'hamburger-react'
@@ -10,32 +10,27 @@ import {FaSearch} from 'react-icons/fa'
 export const Nav = ({mockUser}) => {
   const router = useRouter()
   const user = checkUser()
-  const [showNavOnClick, setShowNavOnClick] = useState(false)
-  const [showNavOnScreenSize, setShowNavOnScreenSize] = useState(true)
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(0)
   const [isOpen, setOpen] = useState(false)
   const [searchInput, setSearchInput] = useState('')
 
-  const toggleMenu = () => {
-    setShowNavOnClick(!showNavOnClick)
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu)
+    setOpen(!isOpen)
   }
 
-  useLayoutEffect(() => {
-    if (window.innerWidth < 768) setShowNavOnScreenSize(false)
-
-    const handleResize = () => {
-      if (window.innerWidth > 768) {
-        setShowNavOnScreenSize(true)
-        setShowNavOnClick(false)
-        setOpen(false)
-      } else if (window.innerWidth < 768) {
-        setShowNavOnScreenSize(false)
-      }
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth)
     }
 
-    window.addEventListener('resize', handleResize)
+    changeWidth()
+
+    window.addEventListener('resize', changeWidth)
 
     return () => {
-      window.removeEventListener('resize', handleResize)
+      window.removeEventListener('resize', changeWidth)
     }
   }, [])
 
@@ -49,7 +44,7 @@ export const Nav = ({mockUser}) => {
 
   return (
     <nav className="flex justify-between py-5 lg:w-10/12 m-auto items-center max-w-[1200px] px-5 lg:px-0 flex-col md:flex-row w-full bg-[#f5f5ee]">
-      <div className="flex flex-col items-center w-max md:flex-row">
+      <div className="flex flex-col items-center w-full lg:w-max md:flex-row">
         <div className="flex items-center justify-between w-full md:w-max md:pr-8">
           {/* Logo */}
           <Link href="/">
@@ -67,7 +62,7 @@ export const Nav = ({mockUser}) => {
           {/* Hamburger button */}
           <button
             data-cy="hamburger-icon"
-            onClick={toggleMenu}
+            onClick={toggleNav}
             className="cursor-pointer md:hidden">
             <Hamburger
               color="rgb(22 101 52)"
@@ -77,126 +72,86 @@ export const Nav = ({mockUser}) => {
           </button>
         </div>
 
-        <div className="flex flex-col w-full md:gap-6 md:flex-row md:w-min">
-          {/* Parks */}
-          <Link href="/parks">
-            <a
-              data-cy="parks"
-              className={`font-display font-bold text-xl cursor-pointer text-green-800 transition-all duration-200 ease-in-out border-b-green-800 border-b border-green-800 md:border-0 md:hover:bg-transparent md:hover:text-green-800 py-4   w-full text-center hover:bg-green-800 hover:text-white lg:pass md:rounded-md  p-4 md:py-2
-                ${
-                  showNavOnScreenSize ||
-                  (showNavOnClick && showNavOnScreenSize != showNavOnClick)
-                    ? 'visible'
-                    : 'hidden'
-                }
-              `}>
-              Parks
-            </a>
-          </Link>
-
-          {/* About */}
-          <Link href="/about">
-            <a
-              data-cy="about-page"
-              className={`font-display font-bold text-xl cursor-pointer text-green-800 transition-all duration-200 ease-in-out border-b-green-800 border-b border-green-800 md:border-0 md:hover:bg-transparent md:hover:text-green-800 py-4   w-full text-center hover:bg-green-800 hover:text-white lg:pass md:rounded-md  p-4 md:py-2
-                ${
-                  showNavOnScreenSize ||
-                  (showNavOnClick && showNavOnScreenSize != showNavOnClick)
-                    ? 'visible'
-                    : 'hidden'
-                }
-              `}>
-              About
-            </a>
-          </Link>
-        </div>
-      </div>
-
-      {/* Search bar */}
-      <form onSubmit={handleSubmit} className="flex gap-4">
-        <input
-          className="px-4 py-2 rounded-md "
-          type="text"
-          placeholder="Search for anything"
-          value={searchInput}
-          name="search"
-          onChange={(e) => setSearchInput(e.target.value)}
-        />
-        <button
-          className="px-4 py-2 font-bold text-white transition-all bg-green-700 border-2 border-transparent rounded-md focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none hover:bg-green-600"
-          type="submit">
-          <FaSearch />
-        </button>
-      </form>
-
-      <div className="flex flex-col items-center text-center w-max md:flex-row md:text-left md:justify-end md:gap-6">
-        {user || mockUser ? (
-          <>
-            {/* Profile */}
-            <Link href="/profile">
+        {(toggleMenu || screenWidth > 768) && (
+          <div className="flex flex-col w-full md:gap-6 md:flex-row md:w-min">
+            {/* Parks */}
+            <Link href="/parks">
               <a
-                className={`font-display font-bold text-xl cursor-pointer text-green-800 transition-all duration-200 ease-in-out md:border-b-0 border-b-green-800 border-b border-green-800 py-4   hover:bg-green-800 hover:text-white md:hover:bg-transparent md:hover:text-green-800 lg:pass md:rounded-md  p-4 md:py-2 
-              ${
-                showNavOnScreenSize ||
-                (showNavOnClick && showNavOnScreenSize != showNavOnClick)
-                  ? 'visible'
-                  : 'hidden'
-              }
-              `}>
-                Profile
+                data-cy="parks"
+                className="w-full p-4 py-4 text-xl font-bold text-center text-green-800 transition-all duration-200 ease-in-out border-b border-green-800 cursor-pointer font-display border-b-green-800 md:border-0 md:hover:bg-transparent md:hover:text-green-800 hover:bg-green-800 hover:text-white lg:pass md:rounded-md md:py-2">
+                Parks
               </a>
             </Link>
 
-            {/* Logout */}
-            <button
-              onClick={() => {
-                Auth.signOut()
-                router.push('/')
-              }}
-              className={`font-display w-max font-bold text-xl cursor-pointer text-green-800 transition-all duration-200 ease-in-out border-b-green-800 border-green-800 py-4 hover:bg-green-800 hover:text-white md:hover:bg-transparent md:rounded-md  p-4 md:py-2 border-2 md:border-green-800 md:hover:bg-green-800 md:hover:text-white focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none
-              ${
-                showNavOnScreenSize ||
-                (showNavOnClick && showNavOnScreenSize != showNavOnClick)
-                  ? 'visible'
-                  : 'hidden'
-              }
-            `}>
-              Log out
-            </button>
-          </>
-        ) : (
-          <>
-            {/* Login */}
-            <Link href="/login">
+            {/* About */}
+            <Link href="/about">
               <a
-                className={`font-display font-bold text-xl cursor-pointer text-green-800 transition-all duration-200 ease-in-out md:border-b-0 border-b-green-800 border-b border-green-800 py-4   hover:bg-green-800 hover:text-white md:hover:bg-transparent md:hover:text-green-800 lg:pass md:rounded-md  p-4 md:py-2 
-              ${
-                showNavOnScreenSize ||
-                (showNavOnClick && showNavOnScreenSize != showNavOnClick)
-                  ? 'visible'
-                  : 'hidden'
-              }
-            `}>
-                Log in
+                data-cy="about-page"
+                className="w-full p-4 py-4 text-xl font-bold text-center text-green-800 transition-all duration-200 ease-in-out border-b border-green-800 cursor-pointer font-display border-b-green-800 md:border-0 md:hover:bg-transparent md:hover:text-green-800 hover:bg-green-800 hover:text-white lg:pass md:rounded-md md:py-2">
+                About
               </a>
             </Link>
-            {/* Signup */}
-            <Link href="/signup">
-              <a
-                className={`font-display font-bold text-xl cursor-pointer text-green-800 transition-all duration-200 ease-in-out border-b-green-800 border-green-800 py-4 hover:bg-green-800 hover:text-white md:hover:bg-transparent md:rounded-md  p-4 md:py-2 border-2 md:border-green-800 md:hover:bg-green-800 md:hover:text-white focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none
-              ${
-                showNavOnScreenSize ||
-                (showNavOnClick && showNavOnScreenSize != showNavOnClick)
-                  ? 'visible'
-                  : 'hidden'
-              }
-            `}>
-                Create Account
-              </a>
-            </Link>
-          </>
+          </div>
         )}
       </div>
+
+      {screenWidth > 1100 && (
+        <form onSubmit={handleSubmit} className="flex gap-4">
+          <input
+            className="px-4 py-2 rounded-md "
+            type="text"
+            placeholder="Search for anything"
+            value={searchInput}
+            name="search"
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <button
+            className="px-4 py-2 font-bold text-white transition-all bg-green-700 border-2 border-transparent rounded-md focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none hover:bg-green-600"
+            type="submit">
+            <FaSearch />
+          </button>
+        </form>
+      )}
+
+      {(toggleMenu || screenWidth > 768) && (
+        <div className="flex flex-col items-center w-full text-center lg:w-max md:flex-row md:text-left md:justify-end md:gap-6">
+          {user || mockUser ? (
+            <>
+              {/* Profile */}
+              <Link href="/profile">
+                <a className="w-full py-4 text-xl font-bold text-green-800 transition-all duration-200 ease-in-out border-b border-green-800 cursor-pointer lg:w-min -4 font-display md:border-b-0 border-b-green-800 hover:bg-green-800 hover:text-white md:hover:bg-transparent md:hover:text-green-800 lg:pass md:rounded-md md:py-2">
+                  Profile
+                </a>
+              </Link>
+
+              {/* Logout */}
+              <button
+                onClick={() => {
+                  Auth.signOut()
+                  router.push('/')
+                }}
+                className="w-full p-4 py-4 text-xl font-bold text-green-800 transition-all duration-200 ease-in-out border-green-800 cursor-pointer md:border-2 lg:w-max font-display border-b-green-800 hover:bg-green-800 hover:text-white md:hover:bg-transparent md:rounded-md md:py-2 md:border-green-800 md:hover:bg-green-800 md:hover:text-white focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
+                Log out
+              </button>
+            </>
+          ) : (
+            <>
+              {/* Login */}
+              <Link href="/login">
+                <a className="p-4 py-4 text-xl font-bold text-green-800 transition-all duration-200 ease-in-out border-green-800 cursor-pointer font-display md:border-b-0 border-b-green-800 hover:bg-green-800 hover:text-white md:hover:bg-transparent md:hover:text-green-800 lg:pass md:rounded-md md:py-2">
+                  Log in
+                </a>
+              </Link>
+              {/* Signup */}
+              <Link href="/signup">
+                <a className="p-4 py-4 text-xl font-bold text-green-800 transition-all duration-200 ease-in-out border-2 border-green-800 cursor-pointer font-display border-b-green-800 hover:bg-green-800 hover:text-white md:hover:bg-transparent md:rounded-md md:py-2 md:border-green-800 md:hover:bg-green-800 md:hover:text-white focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
+                  Create Account
+                </a>
+              </Link>
+            </>
+          )}
+        </div>
+      )}
     </nav>
   )
 }

d2350a72a9af1730220737abfad4e169c7f7cd79
tyler-morales <tyler.morales.dev@gmail.com>
2022-02-12 11:58:06 -0600

    💄 Widen width to 95vw

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ export default function Layout({children, fullWidth}) {
   return (
     <main
       className={`m-auto pb-36 px-5 xl:px-0 lg:mb-12 ${
-        fullWidth ? 'md:max-w-[90vw]' : 'max-w-[1200px] '
+        fullWidth ? 'md:max-w-[95vw]' : 'max-w-[1200px] '
       }`}>
       {children}
     </main>

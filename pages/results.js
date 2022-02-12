5fd8860b3640056d060054f4a0c29b341109e3d7
tyler-morales <tyler.morales.dev@gmail.com>
2022-02-12 12:00:59 -0600

    🏗️  Create 2 column grid structure for sites and map

--- pages/results.js
+++ pages/results.js
@@ -57,55 +57,62 @@ export default function results({parks, params}) {
         <span className="font-bold">{convertValueToLabel(q, ids)}</span>
       </span>
 
-      <section className="grid grid-cols-1 gap-6 md:grid-cols-2">
-        {total > 0 &&
-          data.map(({parkCode, fullName, images}) => (
-            <Link href={`/park/${parkCode}`} key={parkCode}>
-              <a className="p-4 bg-[#fafafa] rounded-lg shadow-lg hover:-translate-y-2 transition-all hover:shadow-xl">
-                <div className="relative h-64">
-                  <Image
-                    layout="fill"
-                    src={images[0].url}
-                    alt={images[0].altText}
-                    className="object-cover w-full rounded-md"
-                  />
-                </div>
-                <h3 className="mt-2 text-2xl font-bold text-green-800">
-                  {fullName}
-                </h3>
-              </a>
-            </Link>
-          ))}
-      </section>
-      {total == 0 && (
-        <section>
-          <h2 className="w-full mt-10 text-3xl font-bold text-center text-green-800">
-            No results, try a different search
-          </h2>
-          <Giphy />
-        </section>
-      )}
-
-      {total > 20 && (
-        <div className="flex justify-center gap-5 mt-12">
-          {start != 0 && (
-            <button
-              onClick={() => childCompRef.current.paginate('decrement')}
-              className="flex items-center justify-center gap-3 py-3 pr-3 text-lg text-center text-white transition-all bg-green-700 rounded-lg shadow-md w-28 hover:bg-green-600 hover:-translate-x-1 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
-              <IoChevronBack />
-              Back
-            </button>
+      <div className="grid grid-cols-2 gap-10">
+        {/* Sites */}
+        <div>
+          <section className="grid grid-cols-1 gap-6 md:grid-cols-2">
+            {total > 0 &&
+              data.map(({parkCode, fullName, images}) => (
+                <Link href={`/park/${parkCode}`} key={parkCode}>
+                  <a className="p-4 bg-[#fafafa] rounded-lg shadow-lg hover:-translate-y-2 transition-all hover:shadow-xl">
+                    <div className="relative h-64">
+                      <Image
+                        layout="fill"
+                        src={images[0].url}
+                        alt={images[0].altText}
+                        className="object-cover w-full rounded-md"
+                      />
+                    </div>
+                    <h3 className="mt-2 text-2xl font-bold text-green-800">
+                      {fullName}
+                    </h3>
+                  </a>
+                </Link>
+              ))}
+          </section>
+          {/* No Results found */}
+          {total == 0 && (
+            <section>
+              <h2 className="w-full mt-10 text-3xl font-bold text-center text-green-800">
+                No results, try a different search
+              </h2>
+              <Giphy />
+            </section>
           )}
-          {total - start > start && (
-            <button
-              onClick={() => childCompRef.current.paginate('increment')}
-              className="flex items-center justify-center gap-3 py-3 pl-3 text-lg text-center text-white transition-all bg-green-700 rounded-lg shadow-md w-28 hover:bg-green-600 hover:translate-x-1 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
-              Next
-              <IoChevronForward />
-            </button>
+
+          {/* Pagination buttons */}
+          {total > 20 && (
+            <div className="flex justify-center gap-5 mt-12">
+              {start != 0 && (
+                <button
+                  onClick={() => childCompRef.current.paginate('decrement')}
+                  className="flex items-center justify-center gap-3 py-3 pr-3 text-lg text-center text-white transition-all bg-green-700 rounded-lg shadow-md w-28 hover:bg-green-600 hover:-translate-x-1 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
+                  <IoChevronBack />
+                  Back
+                </button>
+              )}
+              {total - start > start && (
+                <button
+                  onClick={() => childCompRef.current.paginate('increment')}
+                  className="flex items-center justify-center gap-3 py-3 pl-3 text-lg text-center text-white transition-all bg-green-700 rounded-lg shadow-md w-28 hover:bg-green-600 hover:translate-x-1 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
+                  Next
+                  <IoChevronForward />
+                </button>
+              )}
+            </div>
           )}
         </div>
-      )}
+      </div>
     </Layout>
   )
 }

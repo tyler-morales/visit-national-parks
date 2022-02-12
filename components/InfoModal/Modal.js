1cfe5ce48b51e79f387004f8b5ab3c57b3230170
tyler-morales <tyler.morales.dev@gmail.com>
2022-02-12 11:13:08 -0600

    🚧  Add modal for alerts

---
+++ components/InfoModal/Modal.js
@@ -0,0 +1,45 @@
+import Backdrop from './Backdrop'
+import {GoAlert} from 'react-icons/go'
+
+const Modal = ({handleClose, alerts}) => {
+  return (
+    <Backdrop onClick={handleClose}>
+      <div
+        onClick={(e) => e.stopPropagation()} // Prevent click from closing modal
+        className="max-w-[700px]  bg-orange-100 rounded-lg">
+        <h2 className="px-8 py-4 text-3xl font-bold text-white bg-red-600 rounded-t-md">
+          Alerts
+        </h2>
+        <ul className="p-8">
+          {alerts.map((alert) => {
+            return (
+              <li className="mb-4">
+                <span className="block mb-2 font-bold text-red-600">
+                  {alert.title} | {alert.lastIndexedDate.slice(0, 10)}
+                </span>
+                {alert.description}
+                <a
+                  target="_blank"
+                  href={alert.url}
+                  className="block mt-2 text-blue-600">
+                  More info
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </Backdrop>
+  )
+}
+
+const ModalCloseButton = ({onClick, label}) => (
+  <button
+    className="px-4 py-2 text-lg transition-all rounded-md ring-offset-primary ring-offset-2 focus:ring-error focus:outline-none focus:ring-2 bg-error"
+    type="button"
+    onClick={onClick}>
+    {label}
+  </button>
+)
+
+export default Modal
--- components/ParkPage/Alert/Alert.js
+++ components/ParkPage/Alert/Alert.js
@@ -1,20 +1,31 @@
 import {useState} from 'react'
+import InfoModal from '../../InfoModal/Modal'
+import useModal from '../../../hooks/useModal'
+import {GoAlert} from 'react-icons/go'
 
 export default function Alert({alerts}) {
+  const {modalOpen, close, open} = useModal()
+
   const [alertStatus, setAlertStatus] = useState(true)
 
   return (
     <>
       {alertStatus && (
         <div className="flex items-center justify-between px-4 py-2 mb-4 text-white bg-red-500 rounded-md ">
-          <h2 className="flex items-between">
-            <span className="font-bold">Alert: {alerts[0]?.title}</span> –{' '}
-            <span className="whitespace-nowrap inline-block max-w-[90ch] overflow-hidden text-ellipsis">
+          <h2 className="flex items-between" onClick={() => open()}>
+            <span className="flex items-center gap-2 font-bold cursor-pointer">
+              <GoAlert /> Alert: {alerts[0]?.title}
+            </span>{' '}
+            –{' '}
+            <span className="whitespace-nowrap inline-block max-w-[85ch] overflow-hidden text-ellipsis">
               {alerts[0]?.description}
             </span>
           </h2>
           <button onClick={() => setAlertStatus(false)}>X</button>
         </div>
+      )}
+      {modalOpen && (
+        <InfoModal modalOpen={modalOpen} handleClose={close} alerts={alerts} />
       )}
     </>
   )

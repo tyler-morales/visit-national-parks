a3aa4b3f81949a083f76dcfcff0b63db66618162
tyler-morales <tyler.morales.dev@gmail.com>
2022-02-20 15:16:42 -0600

    🐛  Added mapbox css files Imported css link to mapbox in /results and /park/[parkCode]

--- pages/park/[parkCode].js
+++ pages/park/[parkCode].js
@@ -45,103 +45,86 @@ export default function Park({
   if (router.isFallback) {
     return 'loading...'
   }
+  return (
+    <>
+      <Head>
+        <title>{fullName}</title>
+        <meta property="og:title" content={fullName} key="title" />
+        <meta
+          name="description"
+          content={
+            'Explore ' + fullName + ' and discover the great American outdoors'
+          }></meta>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </Head>
 
-  if (
-    (name,
-    fullName,
-    description,
-    parkCode,
-    designation,
-    images,
-    states,
-    contacts,
-    operatingHours,
-    entranceFees,
-    entrancePasses,
-    latitude,
-    longitude,
-    thingsToDo,
-    alerts)
+      <Layout>
+        {alerts.length > 0 && <Alert alerts={alerts} />}
+        <button
+          onClick={() => router.back()}
+          aria-label="Back to results"
+          className="text-sm text-gray-600-700">
+          ↞Back to Results
+        </button>
+
+        {/* Title */}
+        <span className="block mb-2 text-center">{designation}</span>
+        <h1 className="mb-5 text-5xl font-bold text-center text-green-800 md:text-7xl">
+          {name}
+        </h1>
+
+        {/* Hero Image */}
+        <HeroImage image={images[0]} />
+
+        <hr className="my-12 border-gray-400" />
+
+        {/* Collection */}
+        <CollectionButton
+          name={name}
+          parkCode={parkCode}
+          fullName={fullName}
+          url={images[0]?.url}
+        />
+
+        {/* General Info */}
+        <GeneralInfo
+          description={description}
+          states={states}
+          contacts={contacts}
+          title="Overview"
+        />
+
+        <div className="grid gap-10 p-8 mt-10 border-2 border-green-700 rounded-lg bg-orange-50 col-1 md:grid-cols-4">
+          {/* Hours */}
+          <Hours operatingHours={operatingHours} title="Hours" />
+          <Fees
+            title="Fees & Passes"
+            entranceFees={entranceFees}
+            entrancePasses={entrancePasses}
+          />
+        </div>
+
+        {/* Map */}
+        <MapBox
+          coordinates={{latitude, longitude}}
+          fullName={fullName}
+          parkCode={parkCode}
+          title="Map"
+        />
+
+        {/* Things To Do */}
+        {thingsToDo.length != 0 && (
+          <ThingsToDo thingsToDo={thingsToDo} title="Things To Do" />
+        )}
+
+        {/* Images */}
+        <Images images={images} title="More Images" />
+      </Layout>
+    </>
   )
-    return (
-      <>
-        <Head>
-          <title>{fullName}</title>
-          <meta property="og:title" content={fullName} key="title" />
-          <meta
-            name="description"
-            content={
-              'Explore ' +
-              fullName +
-              ' and discover the great American outdoors'
-            }></meta>
-        </Head>
-
-        <Layout>
-          {alerts.length > 0 && <Alert alerts={alerts} />}
-          <button
-            onClick={() => router.back()}
-            aria-label="Back to results"
-            className="text-sm text-gray-600-700">
-            ↞Back to Results
-          </button>
-
-          {/* Title */}
-          <span className="block mb-2 text-center">{designation}</span>
-          <h1 className="mb-5 text-5xl font-bold text-center text-green-800 md:text-7xl">
-            {name}
-          </h1>
-
-          {/* Hero Image */}
-          <HeroImage image={images[0]} />
-
-          <hr className="my-12 border-gray-400" />
-
-          {/* Collection */}
-          <CollectionButton
-            name={name}
-            parkCode={parkCode}
-            fullName={fullName}
-            url={images[0]?.url}
-          />
-
-          {/* General Info */}
-          <GeneralInfo
-            description={description}
-            states={states}
-            contacts={contacts}
-            title="Overview"
-          />
-
-          <div className="grid gap-10 p-8 mt-10 border-2 border-green-700 rounded-lg bg-orange-50 col-1 md:grid-cols-4">
-            {/* Hours */}
-            <Hours operatingHours={operatingHours} title="Hours" />
-            <Fees
-              title="Fees & Passes"
-              entranceFees={entranceFees}
-              entrancePasses={entrancePasses}
-            />
-          </div>
-
-          {/* Map */}
-          <MapBox
-            coordinates={{latitude, longitude}}
-            fullName={fullName}
-            parkCode={parkCode}
-            title="Map"
-          />
-
-          {/* Things To Do */}
-          {thingsToDo.length != 0 && (
-            <ThingsToDo thingsToDo={thingsToDo} title="Things To Do" />
-          )}
-
-          {/* Images */}
-          <Images images={images} title="More Images" />
-        </Layout>
-      </>
-    )
-  else return <div>Loading...</div>
 }
 
 export async function getStaticPaths() {
--- pages/results.js
+++ pages/results.js
@@ -8,6 +8,8 @@ import Layout from '../components/Layout'
 import {IoChevronBack, IoChevronForward} from 'react-icons/io5'
 import Image from 'next/image'
 
+import Head from 'next/head'
+
 import MapBox from '../components/Map/MapBox'
 
 import {IoMdMap} from 'react-icons/io'
@@ -63,118 +65,130 @@ export default function results({parks, totalParks, params}) {
     }
   }, [])
 
-  if (parks && params)
+  if (parks && params && totalParks) {
     return (
-      <Layout fullWidth>
-        <header className="flex flex-col w-full mb-8 ">
-          <h1 className="my-5 text-5xl font-bold text-green-800">Results</h1>
-          <SearchBar fullSearchBar={false} ref={childCompRef} page={start} />
-        </header>
+      <>
+        <Head>
+          <link
+            href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+            rel="stylesheet"
+          />
+        </Head>
+        <Layout fullWidth>
+          <header className="flex flex-col w-full mb-8 ">
+            <h1 className="my-5 text-5xl font-bold text-green-800">Results</h1>
+            <SearchBar fullSearchBar={false} ref={childCompRef} page={start} />
+          </header>
 
-        <span className="block mb-6 text-xs tracking-wider text-gray-500">
-          {totalParks} results for{' '}
-          <span className="font-bold">
-            {convertValueToLabel(state, states)}
-          </span>
-          {state && q ? ' and ' : ''}
-          <span className="font-bold">{convertValueToLabel(q, ids)}</span>
-          {!params?.q?.includes('-') && !params?.state && (
-            <span>
-              {params?.q?.charAt(0).toUpperCase() + params?.q?.slice(1)}
+          <span className="block mb-6 text-xs tracking-wider text-gray-500">
+            {totalParks} results for{' '}
+            <span className="font-bold">
+              {convertValueToLabel(state, states)}
             </span>
-          )}
-        </span>
+            {state && q ? ' and ' : ''}
+            <span className="font-bold">{convertValueToLabel(q, ids)}</span>
+            {!params?.q?.includes('-') && !params?.state && (
+              <span>
+                {params?.q?.charAt(0).toUpperCase() + params?.q?.slice(1)}
+              </span>
+            )}
+          </span>
 
-        {/* Buttons to change view */}
-        {screenWidth < 768 && (
-          <div className="fixed z-50 flex justify-center gap-4 bottom-10">
-            <button
-              onClick={() => setToggleView('LIST')}
-              className="transition-all hover:-translate-y-1">
-              <div className="flex items-center gap-2 px-4 py-2 text-black bg-white rounded-md shadow-sm ">
-                <AiOutlineUnorderedList size="1.25em" />
-                <span>View List</span>
-              </div>
-            </button>
-            <button
-              onClick={() => setToggleView('MAP')}
-              className="transition-all hover:-translate-y-1">
-              <div className="flex items-center gap-2 px-4 py-2 text-black bg-white rounded-md shadow-sm ">
-                <IoMdMap size="1.25em" />
-                <span>View Map</span>
-              </div>
-            </button>
-          </div>
-        )}
-
-        <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
-          {/* Sites */}
-          {(toggleView == 'LIST' || screenWidth > 768) && (
-            <div>
-              <section className="grid grid-cols-1 gap-6 lg:grid-cols-2">
-                {totalParks > 0 &&
-                  parks?.map(
-                    ({parkCode, fullName, imageUrl, imageAlt}, index) => (
-                      <Link href={`/park/${parkCode}`} key={index}>
-                        <a className="p-4 bg-[#fafafa] rounded-lg shadow-lg hover:-translate-y-2 transition-all hover:shadow-xl">
-                          <div className="relative h-64">
-                            <Image
-                              layout="fill"
-                              src={imageUrl}
-                              alt={imageAlt}
-                              className="object-cover w-full rounded-md"
-                            />
-                          </div>
-                          <h3 className="mt-2 text-2xl font-bold text-green-800">
-                            {fullName}
-                          </h3>
-                        </a>
-                      </Link>
-                    )
-                  )}
-              </section>
-              {/* No Results found */}
-              {totalParks == 0 && (
-                <section>
-                  <h2 className="w-full mt-10 text-3xl font-bold text-center text-green-800">
-                    No results, try a different search
-                  </h2>
-                  <Giphy />
-                </section>
-              )}
-
-              {/* Pagination buttons */}
-              {totalParks > 20 && (
-                <div className="flex justify-center gap-5 mt-12">
-                  {start != 0 && (
-                    <button
-                      onClick={() => childCompRef.current.paginate('decrement')}
-                      className="flex items-center justify-center gap-3 py-3 pr-3 text-lg text-center text-white transition-all bg-green-700 rounded-lg shadow-md w-28 hover:bg-green-600 hover:-translate-x-1 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
-                      <IoChevronBack />
-                      Back
-                    </button>
-                  )}
-                  {totalParks - start > start && (
-                    <button
-                      onClick={() => childCompRef.current.paginate('increment')}
-                      className="flex items-center justify-center gap-3 py-3 pl-3 text-lg text-center text-white transition-all bg-green-700 rounded-lg shadow-md w-28 hover:bg-green-600 hover:translate-x-1 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
-                      Next
-                      <IoChevronForward />
-                    </button>
-                  )}
+          {/* Buttons to change view */}
+          {screenWidth < 768 && (
+            <div className="fixed z-50 flex justify-center gap-4 bottom-10">
+              <button
+                onClick={() => setToggleView('LIST')}
+                className="transition-all hover:-translate-y-1">
+                <div className="flex items-center gap-2 px-4 py-2 text-black bg-white rounded-md shadow-sm ">
+                  <AiOutlineUnorderedList size="1.25em" />
+                  <span>View List</span>
                 </div>
-              )}
+              </button>
+              <button
+                onClick={() => setToggleView('MAP')}
+                className="transition-all hover:-translate-y-1">
+                <div className="flex items-center gap-2 px-4 py-2 text-black bg-white rounded-md shadow-sm ">
+                  <IoMdMap size="1.25em" />
+                  <span>View Map</span>
+                </div>
+              </button>
             </div>
           )}
 
-          {/* Map */}
-          {(toggleView == 'MAP' || screenWidth > 768) && (
-            <MapBox coordinates={parks} />
-          )}
-        </div>
-      </Layout>
+          <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+            {/* Sites */}
+            {(toggleView == 'LIST' || screenWidth > 768) && (
+              <div>
+                <section className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+                  {totalParks > 0 &&
+                    parks?.map(
+                      ({parkCode, fullName, imageUrl, imageAlt}, index) => (
+                        <Link href={`/park/${parkCode}`} key={index}>
+                          <a className="p-4 bg-[#fafafa] rounded-lg shadow-lg hover:-translate-y-2 transition-all hover:shadow-xl">
+                            <div className="relative h-64">
+                              <Image
+                                layout="fill"
+                                src={imageUrl}
+                                alt={imageAlt}
+                                className="object-cover w-full rounded-md"
+                              />
+                            </div>
+                            <h3 className="mt-2 text-2xl font-bold text-green-800">
+                              {fullName}
+                            </h3>
+                          </a>
+                        </Link>
+                      )
+                    )}
+                </section>
+                {/* No Results found */}
+                {totalParks == 0 && (
+                  <section>
+                    <h2 className="w-full mt-10 text-3xl font-bold text-center text-green-800">
+                      No results, try a different search
+                    </h2>
+                    <Giphy />
+                  </section>
+                )}
+
+                {/* Pagination buttons */}
+                {totalParks > 20 && (
+                  <div className="flex justify-center gap-5 mt-12">
+                    {start != 0 && (
+                      <button
+                        onClick={() =>
+                          childCompRef.current.paginate('decrement')
+                        }
+                        className="flex items-center justify-center gap-3 py-3 pr-3 text-lg text-center text-white transition-all bg-green-700 rounded-lg shadow-md w-28 hover:bg-green-600 hover:-translate-x-1 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
+                        <IoChevronBack />
+                        Back
+                      </button>
+                    )}
+                    {totalParks - start > start && (
+                      <button
+                        onClick={() =>
+                          childCompRef.current.paginate('increment')
+                        }
+                        className="flex items-center justify-center gap-3 py-3 pl-3 text-lg text-center text-white transition-all bg-green-700 rounded-lg shadow-md w-28 hover:bg-green-600 hover:translate-x-1 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-2 focus-visible:outline-blue-500 focus:transition-none">
+                        Next
+                        <IoChevronForward />
+                      </button>
+                    )}
+                  </div>
+                )}
+              </div>
+            )}
+
+            {/* Map */}
+            {(toggleView == 'MAP' || screenWidth > 768) && (
+              <MapBox coordinates={parks} />
+            )}
+          </div>
+        </Layout>
+      </>
     )
-  else return <div>Loading...</div>
+  } else return <div>Loading...</div>
 }
 // && coordinates.length > 0
 

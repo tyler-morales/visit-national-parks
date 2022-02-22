5f7d4a79702f3d6ff696e2e6ac4dd2dfbd2a70ce
Tyler Morales <44333523+tyler-morales@users.noreply.github.com>
2022-02-22 13:03:43 -0600

    🐛 Bug/fetch data (#37)
    * ♻️  Refactor home page to catch undefined errors
    
    * 🗑️  Clean up code on parkCode page

--- pages/index.js
+++ pages/index.js
@@ -19,56 +19,66 @@ export default function Home({nationalParks, parkNames}) {
 }
 
 export async function getStaticProps() {
+  //  Set the base URL for the API
   const URL = 'https://developer.nps.gov/api/v1/'
 
-  // Call API Data for /PARK
-  const res = await fetch(
-    `${URL}parks?&limit=465&api_key=${process.env.API_KEY}`,
-    {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json, text/plain, */*',
-        'User-Agent': '*',
-      },
-    }
-  )
+  // Set data to null to handle errors
+  let nationalParks = null
+  let parkNames = null
 
-  const parksData = await res.json()
-
-  if (!parksData) {
-    return {notFound: true}
+  // Extract method and header from fetch
+  const reqBody = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'User-Agent': '*',
+    },
   }
 
-  const parks = parksData?.data
-
-  // Filter and map for Only the National Parks
-  const nationalParks = parks
-    .filter(
-      (park) =>
-        park.designation == 'National Park' ||
-        park.designation == 'National Park & Preserve' ||
-        park.designation == 'National Parks' ||
-        park.designation == 'National Park and Preserve' ||
-        park.designation == 'National and State Parks' ||
-        park.name == 'National Park of American Samoa'
+  // Call API Data for /PARK
+  try {
+    const res = await fetch(
+      `${URL}parks?&limit=465&api_key=${process.env.API_KEY}`,
+      reqBody
     )
-    .map((park) => {
+
+    const data = await res.json()
+    nationalParks = data?.data
+
+    // Filter and map for Only the National Parks
+    nationalParks = nationalParks
+      .filter(
+        (park) =>
+          park.designation == 'National Park' ||
+          park.designation == 'National Park & Preserve' ||
+          park.designation == 'National Parks' ||
+          park.designation == 'National Park and Preserve' ||
+          park.designation == 'National and State Parks' ||
+          park.name == 'National Park of American Samoa'
+      )
+      .map((park) => {
+        return {
+          name: park.name,
+          parkCode: park.parkCode,
+          image: park.images[0],
+        }
+      })
+
+    parkNames = data?.data
+
+    // Map over each park and return the park name and park code
+    parkNames = parkNames.map((park) => {
       return {
         name: park.name,
-        parkCode: park.parkCode,
-        image: park.images[0],
+        code: park.parkCode,
       }
     })
+  } catch (err) {
+    console.error(err)
+  }
 
-  // Map over each park and return the park name and park code
-  const parkNames = parks.map((park) => {
-    return {
-      name: park.name,
-      code: park.parkCode,
-    }
-  })
   // console.log('********************************')
-  // console.log(nationalParks.length)
+  // console.log(nationalParks?.length, parkNames.length)
 
   return {
     props: {

5a2c1efba41ccf9ab81f2593792ce6ceeb4eb7f0
tyler-morales <tyler.morales.dev@gmail.com>
2022-02-24 17:42:05 -0600

    ♻️  Refactor Infinite scroller by extracting row and images

--- components/InfiniteImages/InfiniteImages.js
+++ components/InfiniteImages/InfiniteImages.js
@@ -4,58 +4,48 @@ const images = [1, 2, 3, 4, 5, 6, 7, 8]
 const imagesReversed = [16, 15, 14, 13, 12, 11, 10, 9]
 
 export default function InfiniteImages() {
+  // Create an Image for each column of images
+  const createImage = (image, direction, index, position) => {
+    return (
+      <Image
+        width={200}
+        height={250}
+        layout="fixed"
+        className="object-cover rounded-lg shadow-lg"
+        // Reverse the order after the initial set of images has rendered (the index parameter reverses the order & position parameter reverses the position)
+        src={`/images/infinite-slider/img${
+          direction == 'forward'
+            ? image + index + position
+            : image - index - position
+        }.jpeg`}
+      />
+    )
+  }
+
+  // Map over images and return a column of 2 images
+  const createImageColumn = (array, direction) => {
+    return array.map((image, index) => (
+      // Create a column of two stacked images
+      <div
+        key={index}
+        // Offset every other column by 20px
+        className={`flex gap-6 md:flex-col ${image % 2 == 0 && 'mt-8'}`}>
+        {createImage(image, direction, index, 0)}
+        {createImage(image, direction, index, 1)}
+      </div>
+    ))
+  }
+
   return (
     <div className="infinite-slider">
+      {/* Set of images */}
       <div className="relative flex justify-around gap-5 infinite-scroller">
-        {/* Images */}
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`flex gap-6 md:flex-col ${
-              image % 2 == 0 ? '' : 'mt-8'
-            }`}>
-            <Image
-              width={200}
-              height={250}
-              layout="fixed"
-              className="object-cover rounded-lg shadow-lg"
-              src={`/images/infinite-slider/img${image + index}.jpeg`}
-            />
-            <Image
-              width={200}
-              height={250}
-              layout="fixed"
-              className="object-cover rounded-lg shadow-lg"
-              src={`/images/infinite-slider/img${image + index + 1}.jpeg`}
-            />
-          </div>
-        ))}
+        {createImageColumn(images, 'forward')}
       </div>
-      {/* Reversed images row*/}
+
+      {/* Set of reversed images (this set creates an illusion of an infinite loop) */}
       <div className="relative flex flex-row-reverse justify-around gap-5 ml-8 infinite-scroller">
-        {/* Images */}
-        {imagesReversed.map((image, index) => (
-          <div
-            key={index}
-            className={`flex gap-6 md:flex-col ${
-              image % 2 == 0 ? '' : 'mt-8'
-            }`}>
-            <Image
-              width={200}
-              height={250}
-              layout="fixed"
-              className="object-cover rounded-lg shadow-lg"
-              src={`/images/infinite-slider/img${image - index}.jpeg`}
-            />
-            <Image
-              width={200}
-              height={250}
-              layout="fixed"
-              className="object-cover rounded-lg shadow-lg"
-              src={`/images/infinite-slider/img${image - index - 1}.jpeg`}
-            />
-          </div>
-        ))}
+        {createImageColumn(imagesReversed, 'reversed')}
       </div>
     </div>
   )

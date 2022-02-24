39382efffb94f92b40834be1d36beb2cc7e65910
tyler-morales <tyler.morales.dev@gmail.com>
2022-02-24 17:17:10 -0600

    ♻️ Refactor infinite scroll component Dyamically render static images from public folder

--- components/InfiniteImages/InfiniteImages.js
+++ components/InfiniteImages/InfiniteImages.js
@@ -1,269 +1,61 @@
 import Image from 'next/image'
 
+const images = [1, 2, 3, 4, 5, 6, 7, 8]
+const imagesReversed = [16, 15, 14, 13, 12, 11, 10, 9]
+
 export default function InfiniteImages() {
   return (
     <div className="infinite-slider">
       <div className="relative flex justify-around gap-5 infinite-scroller">
         {/* Images */}
-        <div className="flex gap-6 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img1.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img2.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:mt-8 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img3.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img4.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img5.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img6.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:mt-8 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img7.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img8.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img9.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img10.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:mt-8 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img11.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img12.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img13.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img14.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:mt-8 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img15.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img16.jpeg"
-          />
-        </div>
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`flex gap-6 md:flex-col ${
+              image % 2 == 0 ? '' : 'mt-8'
+            }`}>
+            <Image
+              width={200}
+              height={250}
+              layout="fixed"
+              className="object-cover rounded-lg shadow-lg"
+              src={`/images/infinite-slider/img${image + index}.jpeg`}
+            />
+            <Image
+              width={200}
+              height={250}
+              layout="fixed"
+              className="object-cover rounded-lg shadow-lg"
+              src={`/images/infinite-slider/img${image + index + 1}.jpeg`}
+            />
+          </div>
+        ))}
       </div>
+      {/* Reversed images row*/}
       <div className="relative flex flex-row-reverse justify-around gap-5 ml-8 infinite-scroller">
         {/* Images */}
-        <div className="flex gap-6 md:mt-8 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img16.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img15.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img14.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img13.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:mt-8 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img12.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img11.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img10.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img9.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:mt-8 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img8.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img7.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img6.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img5.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:mt-8 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img4.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img3.jpeg"
-          />
-        </div>
-        <div className="flex gap-6 md:flex-col">
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img2.jpeg"
-          />
-          <Image
-            width={200}
-            height={250}
-            layout="fixed"
-            className="object-cover rounded-lg shadow-lg"
-            src="/images/infinite-slider/img1.jpeg"
-          />
-        </div>
+        {imagesReversed.map((image, index) => (
+          <div
+            key={index}
+            className={`flex gap-6 md:flex-col ${
+              image % 2 == 0 ? '' : 'mt-8'
+            }`}>
+            <Image
+              width={200}
+              height={250}
+              layout="fixed"
+              className="object-cover rounded-lg shadow-lg"
+              src={`/images/infinite-slider/img${image - index}.jpeg`}
+            />
+            <Image
+              width={200}
+              height={250}
+              layout="fixed"
+              className="object-cover rounded-lg shadow-lg"
+              src={`/images/infinite-slider/img${image - index - 1}.jpeg`}
+            />
+          </div>
+        ))}
       </div>
     </div>
   )

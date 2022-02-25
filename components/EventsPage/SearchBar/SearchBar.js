a71cb59611b16e28255a955ededb9782faf4f467
tyler-morales <tyler.morales.dev@gmail.com>
2022-02-25 08:42:51 -0600

    ♻️ Refactor form submission block Extract form and pass in arguments to create the State and Park form

--- components/EventsPage/SearchBar/SearchBar.js
+++ components/EventsPage/SearchBar/SearchBar.js
@@ -24,7 +24,7 @@ const months = [
 export default function SearchBar() {
   const [tab, setTab] = useState('STATE')
   const [selectedState, setselectedState] = useState(null)
-  const [selectedPark, setselectedPark] = useState(null)
+  const [selectedPark, setSelectedPark] = useState(null)
   const [editStartDate, setEditStartDate] = useState(false)
 
   const [date, setDate] = useState({
@@ -38,12 +38,14 @@ export default function SearchBar() {
   const handleSubmitByState = (e) => {
     e.preventDefault()
     setEditStartDate(false)
+    console.log('state')
     //    Call NPS API???
   }
 
   const handleSubmitByPark = (e) => {
     e.preventDefault()
-    console.log('clicked park')
+    setEditStartDate(false)
+    console.log('park')
   }
 
   const changeYear = (e) => {
@@ -95,6 +97,88 @@ export default function SearchBar() {
     ))
   }
 
+  const formBuilder = (
+    name,
+    options,
+    selectedOption,
+    setSelectedOption,
+    submitter
+  ) => {
+    return (
+      <>
+        <form
+          onSubmit={(e) => submitter(e)}
+          className="relative flex flex-col items-center justify-between w-full gap-6 px-4 py-4 m-auto mt-6 bg-white border-2 border-green-900 rounded-lg shadow-lg md:rounded-3xl md:gap-8 md:flex-row md:px-12 md:w-min">
+          {/* Select State */}
+          <div className="w-full md:w-[200px] flex flex-col justify-between md:border-r-2 md:border-gray-200 pr-6">
+            <label className="mb-2 text-xs">{name}</label>
+            <Select
+              options={options}
+              value={selectedOption}
+              onChange={setSelectedOption}
+              instanceId={name}
+            />
+          </div>
+
+          {/* Selecet Date */}
+          <div className="w-full relative md:w-[180px] flex flex-col justify-between pr-6 md:border-r-2 md:border-gray-200">
+            <label className="block mb-2 text-xs">Date</label>
+            <button
+              type="button"
+              className="px-2 py-2 text-left border border-gray-400 rounded-md"
+              onClick={() => setEditStartDate(editStartDate ? false : true)}>
+              {date.month.slice(0, 3)} / {date.day} / {date.year}
+            </button>
+          </div>
+
+          {/* Search */}
+          <button
+            type="submit"
+            className="flex justify-center w-full px-5 py-5 text-white bg-green-800 rounded-full md:w-min">
+            <FaSearch size="1.25em" />
+          </button>
+        </form>
+
+        {editStartDate && (
+          <div className="flex flex-col justify-center max-w-screen-sm gap-2 m-auto mt-4 rounded-md md:gap-4 md:flex-row">
+            {/* Month */}
+            <select
+              onChange={(e) => changeMonth(e)}
+              className="py-2 px-4 font-sans bg-white w-full md:w-[130px] x-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              name="Month">
+              <option value={date.month} className="px-4 py-2 rounded-md">
+                {date.month}
+              </option>
+              <CreateMonthOptions />
+            </select>
+
+            {/* Day */}
+            <select
+              onChange={(e) => changeDay(e)}
+              className="pl-2 py-2 font-sans bg-white w-full md:w-[80px] rounded-md md:rounded-r-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              name="Day">
+              <option value={date.day} className="px-4 py-2 rounded-md">
+                {date.day}
+              </option>
+              <CreateDayOptions />
+            </select>
+
+            {/* Year */}
+            <select
+              onChange={(e) => changeYear(e)}
+              className="pl-2 py-2 font-sans bg-white w-full md:w-[100px] rounded-md md:rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              name="Year">
+              <option value={date.year} className="px-4 py-2 rounded-md">
+                {date.year}
+              </option>
+              <CreateYearOptions />
+            </select>
+          </div>
+        )}
+      </>
+    )
+  }
+
   return (
     <>
       <div className="m-auto max-w-[1200px] my-10 px-4">
@@ -118,157 +202,24 @@ export default function SearchBar() {
         </div>
 
         {/* Search by STATE */}
-        {tab == 'STATE' && (
-          <>
-            <form
-              onSubmit={(e) => handleSubmitByState(e)}
-              className="relative flex flex-col items-center justify-between w-full gap-6 px-4 py-4 m-auto mt-6 bg-white border-2 border-green-900 rounded-md md:gap-8 md:flex-row md:px-12 md:w-min">
-              {/* Select State */}
-              <div className="w-full md:w-[200px] flex flex-col justify-between md:border-r-2 md:border-gray-200 pr-6">
-                <label className="mb-2 text-xs">State</label>
-                <Select
-                  options={states}
-                  value={selectedState}
-                  onChange={setselectedState}
-                  instanceId="states"
-                />
-              </div>
-
-              {/* Selecet Date */}
-              <div className="w-full relative md:w-[180px] flex flex-col justify-between pr-6 md:border-r-2 md:border-gray-200">
-                <label className="block mb-2 text-xs">Date</label>
-                <button
-                  type="button"
-                  className="px-2 py-2 text-left border border-gray-400 rounded-md"
-                  onClick={() =>
-                    setEditStartDate(editStartDate ? false : true)
-                  }>
-                  {date.month.slice(0, 3)} / {date.day} / {date.year}
-                </button>
-              </div>
-
-              {/* Search */}
-              <button
-                type="submit"
-                className="flex justify-center w-full px-5 py-5 text-white bg-green-800 rounded-full md:w-min">
-                <FaSearch size="1.25em" />
-              </button>
-            </form>
-
-            {editStartDate && (
-              <div className="flex flex-col justify-center max-w-screen-sm gap-2 m-auto mt-4 rounded-md md:gap-4 md:flex-row">
-                {/* Month */}
-                <select
-                  onChange={(e) => changeMonth(e)}
-                  className="py-2 px-4 font-sans bg-white w-full md:w-[130px] x-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  name="Month">
-                  <option value={date.month} className="px-4 py-2 rounded-md">
-                    {date.month}
-                  </option>
-                  <CreateMonthOptions />
-                </select>
-
-                {/* Day */}
-                <select
-                  onChange={(e) => changeDay(e)}
-                  className="pl-2 py-2 font-sans bg-white w-full md:w-[80px] rounded-md md:rounded-r-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  name="Day">
-                  <option value={date.day} className="px-4 py-2 rounded-md">
-                    {date.day}
-                  </option>
-                  <CreateDayOptions />
-                </select>
-
-                {/* Year */}
-                <select
-                  onChange={(e) => changeYear(e)}
-                  className="pl-2 py-2 font-sans bg-white w-full md:w-[100px] rounded-md md:rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  name="Year">
-                  <option value={date.year} className="px-4 py-2 rounded-md">
-                    {date.year}
-                  </option>
-                  <CreateYearOptions />
-                </select>
-              </div>
-            )}
-          </>
-        )}
+        {tab == 'STATE' &&
+          formBuilder(
+            'State',
+            states,
+            selectedState,
+            setSelectedPark,
+            handleSubmitByState
+          )}
 
         {/* Search by PARK */}
-        {tab == 'PARK' && (
-          <>
-            <form className="relative flex flex-col items-center justify-between w-full gap-6 px-4 py-4 m-auto mt-6 bg-white border-2 border-green-900 rounded-md md:gap-8 md:flex-row md:px-12 md:w-min">
-              {/* Select Park */}
-              <div className="w-full md:w-[200px] flex flex-col justify-between md:border-r-2 md:border-gray-200 pr-6">
-                <label className="mb-2 text-xs">Park</label>
-                <Select
-                  options={parks}
-                  value={selectedPark}
-                  onChange={setselectedPark}
-                  instanceId="parks"
-                  className="cursor-text"
-                />
-              </div>
-
-              {/* Selecet Date */}
-              <div className="w-full relative md:w-[180px] flex flex-col justify-between pr-6 md:border-r-2 md:border-gray-200">
-                <label className="block mb-2 text-xs">Date</label>
-                <button
-                  type="button"
-                  className="px-2 py-2 text-left border border-gray-400 rounded-md"
-                  onClick={() =>
-                    setEditStartDate(editStartDate ? false : true)
-                  }>
-                  {date.month.slice(0, 3)} / {date.day} / {date.year}
-                </button>
-              </div>
-
-              {/* Search */}
-              <button
-                type="submit"
-                className="flex justify-center w-full px-5 py-5 text-white bg-green-800 rounded-full md:w-min">
-                <FaSearch size="1.25em" />
-              </button>
-            </form>
-
-            {editStartDate && (
-              <div className="flex flex-col justify-center max-w-screen-sm gap-2 m-auto mt-4 rounded-md md:gap-4 md:flex-row">
-                {/* Month */}
-                <select
-                  onChange={(e) => changeMonth(e)}
-                  className="py-2 px-4 font-sans bg-white w-full md:w-[130px] x-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  name="Month">
-                  <option value={date.month} className="px-4 py-2 rounded-md">
-                    {date.month}
-                  </option>
-                  <CreateMonthOptions />
-                </select>
-
-                {/* Day */}
-                <select
-                  onChange={(e) => changeDay(e)}
-                  className="pl-2 py-2 font-sans bg-white w-full md:w-[80px] rounded-md md:rounded-r-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  name="Day">
-                  <option value={date.day} className="px-4 py-2 rounded-md">
-                    {date.day}
-                  </option>
-                  <CreateDayOptions />
-                </select>
-
-                {/* Year */}
-                <select
-                  onChange={(e) => changeYear(e)}
-                  className="pl-2 py-2 font-sans bg-white w-full md:w-[100px] rounded-md md:rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  name="Year">
-                  <option value={date.year} className="px-4 py-2 rounded-md">
-                    {date.year}
-                  </option>
-                  <CreateYearOptions />
-                </select>
-              </div>
-            )}
-          </>
-        )}
+        {tab == 'PARK' &&
+          formBuilder(
+            'Park',
+            parks,
+            selectedPark,
+            setSelectedPark,
+            handleSubmitByPark
+          )}
       </div>
     </>
   )

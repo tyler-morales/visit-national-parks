ae6b22f7515b4ccf1a795ca6f21846f236298d9b
tyler-morales <tyler.morales.dev@gmail.com>
2022-02-26 11:47:39 -0600

    🔒️  Update mapbox API key

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     API_KEY: process.env.API_KEY,
+    NEXT_PUBLIC_MAPBOX_API_KEY: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
     MAPBOX_API_KEY: process.env.MAPBOX_API_KEY,
   },
   images: {

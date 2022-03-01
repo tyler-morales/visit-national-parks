ae2f2d44cba848679c25f5276dee9cbeb414b8a6
tyler-morales <tyler.morales.dev@gmail.com>
2022-03-01 13:44:06 -0600

    ✨ Render two column layout Map and selected Park

--- components/ParksPage/Map/Map.js
+++ components/ParksPage/Map/Map.js
@@ -3,7 +3,7 @@ import MapGL, {Marker, Popup} from 'react-map-gl'
 
 import {FaMapMarkerAlt} from 'react-icons/fa'
 
-export default function MapBox({coordinates, width}) {
+export default function MapBox({parks, passMapData, width}) {
   const [viewport, setViewport] = useState({
     longitude: -100,
     latitude: 40,
@@ -18,15 +18,23 @@ export default function MapBox({coordinates, width}) {
     })
   }, [])
 
+  const handleClickedPark = (code) => {
+    passMapData(code)
+  }
+
   const pins = useMemo(
     () =>
-      coordinates.map((coordinate, index) => (
+      parks.map((coordinate, index) => (
         <Marker
           key={`marker-${index}`}
           longitude={+coordinate.longitude}
           latitude={+coordinate.latitude}
           anchor="bottom">
-          <FaMapMarkerAlt size="2em" className="text-blue-700 cursor-pointer" />
+          <FaMapMarkerAlt
+            size="2em"
+            className="text-blue-700 cursor-pointer"
+            onClick={() => handleClickedPark(coordinate.parkCode)}
+          />
         </Marker>
       )),
     []
--- pages/parks.js
+++ pages/parks.js
@@ -1,14 +1,90 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import MapBox from '../components/ParksPage/Map/Map'
 import Layout from '../components/Layout'
+import Image from 'next/image'
+import Link from 'next/link'
+import {FiLink} from 'react-icons/fi'
+
+// Write state codes and state name
+const stateCodes = [
+  {code: 'AL', name: 'Alabama'},
+  {code: 'AK', name: 'Alaska'},
+  {code: 'AZ', name: 'Arizona'},
+  {code: 'AR', name: 'Arkansas'},
+  {code: 'CA', name: 'California'},
+  {code: 'CO', name: 'Colorado'},
+  {code: 'CT', name: 'Connecticut'},
+  {code: 'DE', name: 'Delaware'},
+  {code: 'DC', name: 'District Of Columbia'},
+  {code: 'FL', name: 'Florida'},
+  {code: 'GA', name: 'Georgia'},
+  {code: 'HI', name: 'Hawaii'},
+  {code: 'ID', name: 'Idaho'},
+  {code: 'IL', name: 'Illinois'},
+  {code: 'IN', name: 'Indiana'},
+  {code: 'IA', name: 'Iowa'},
+  {code: 'KS', name: 'Kansas'},
+  {code: 'KY', name: 'Kentucky'},
+  {code: 'LA', name: 'Louisiana'},
+  {code: 'ME', name: 'Maine'},
+  {code: 'MD', name: 'Maryland'},
+  {code: 'MA', name: 'Massachusetts'},
+  {code: 'MI', name: 'Michigan'},
+  {code: 'MN', name: 'Minnesota'},
+  {code: 'MS', name: 'Mississippi'},
+  {code: 'MO', name: 'Missouri'},
+  {code: 'MT', name: 'Montana'},
+  {code: 'NE', name: 'Nebraska'},
+  {code: 'NV', name: 'Nevada'},
+  {code: 'NH', name: 'New Hampshire'},
+  {code: 'NJ', name: 'New Jersey'},
+  {code: 'NM', name: 'New Mexico'},
+  {code: 'NY', name: 'New York'},
+  {code: 'NC', name: 'North Carolina'},
+  {code: 'ND', name: 'North Dakota'},
+  {code: 'OH', name: 'Ohio'},
+  {code: 'OK', name: 'Oklahoma'},
+  {code: 'OR', name: 'Oregon'},
+  {code: 'PA', name: 'Pennsylvania'},
+  {code: 'RI', name: 'Rhode Island'},
+  {code: 'SC', name: 'South Carolina'},
+  {code: 'SD', name: 'South Dakota'},
+  {code: 'TN', name: 'Tennessee'},
+  {code: 'TX', name: 'Texas'},
+  {code: 'UT', name: 'Utah'},
+  {code: 'VT', name: 'Vermont'},
+  {code: 'VA', name: 'Virginia'},
+  {code: 'WA', name: 'Washington'},
+  {code: 'WV', name: 'West Virginia'},
+  {code: 'WI', name: 'Wisconsin'},
+  {code: 'WY', name: 'Wyoming'},
+  {code: 'VI', name: 'Virgin Islands'},
+]
+
+// Convert state code to state name
+const stateName = (code) => {
+  return stateCodes.find((state) => state.code === code).name
+}
 
 export default function Parks({nationalParks}) {
   const [mapWidth, setMapWidth] = useState(true)
   const [selectedPark, setSelectedPark] = useState(null)
 
+  useEffect(() => {
+    if (selectedPark != null) {
+      setMapWidth(false)
+    } else {
+      setMapWidth(true)
+    }
+  }, [selectedPark])
+
   if (nationalParks) {
-    const coordinates = nationalParks.map((park) => {
-      return {latitude: park.latitude, longitude: park.longitude}
+    const parks = nationalParks.map((park) => {
+      return {
+        latitude: park.latitude,
+        longitude: park.longitude,
+        parkCode: park.parkCode,
+      }
     })
 
     return (
@@ -16,9 +92,59 @@ export default function Parks({nationalParks}) {
         <h1 className="mt-8 text-3xl font-bold text-center text-green-800 md:text-6xl">
           Official National Parks
         </h1>
-        <div className="grid grid-cols-3 mt-8">
-          <MapBox coordinates={coordinates} width={mapWidth} />
-          {selectedPark && <div className="bg-gray-300">Park Name</div>}
+        <div className="grid grid-cols-3 gap-5 mt-8">
+          <MapBox
+            parks={parks}
+            width={mapWidth}
+            passMapData={setSelectedPark}
+          />
+          {selectedPark != null && (
+            <div className="p-6 bg-gray-100 border-2 border-green-800 rounded-lg shadow-md">
+              {nationalParks
+                .filter((park) => park.parkCode == selectedPark)
+                .map((park) => {
+                  return (
+                    <div className="flex flex-col justify-between h-full">
+                      <div>
+                        <h2 className="mb-2 text-3xl font-bold text-center text-green-900">
+                          {park.name}
+                        </h2>
+                        <Image
+                          height={350}
+                          width={600}
+                          src={park.image.url}
+                          alt={park.image.altText}
+                          Layout="responsive"
+                          className="object-cover w-full rounded-md"
+                        />
+                        <span className="block mt-4 text-sm text-gray-400 uppercase">
+                          description
+                        </span>
+                        <p className="mt-2 font-display">{park.description}</p>
+
+                        <span className="block mt-4 text-sm text-gray-400 uppercase">
+                          States
+                        </span>
+                        <p className="mt-2 font-display">
+                          {park.states.length == 2
+                            ? stateName(park.states)
+                            : park.states
+                                .split(',')
+                                .map((state, index) => stateName(state) + ' ')}
+                        </p>
+                      </div>
+
+                      <Link href={`/park/${park.parkCode}`}>
+                        <a className="flex gap-2 text-blue-600">
+                          <FiLink size="1.25em" />
+                          <span>More Info</span>
+                        </a>
+                      </Link>
+                    </div>
+                  )
+                })}
+            </div>
+          )}
         </div>
       </Layout>
     )
@@ -70,6 +196,7 @@ export async function getStaticProps() {
           parkCode: park.parkCode,
           image: park.images[0],
           description: park.description,
+          states: park.states,
           latitude: park.latitude,
           longitude: park.longitude,
         }
